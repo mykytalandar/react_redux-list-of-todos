0bdb781550ca51e995a9411de591c578bb0ac321
Mykyta Landar <mykytalandar@gmail.com>
2025-09-27 02:43:05 +0200

    create task

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,60 @@
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import { Loader, TodoFilter, TodoList, TodoModal } from './components';
+import { useEffect, useState } from 'react';
+import { getTodos } from './api';
+import { todosSlice } from './features/todos';
+import { useAppDispatch, useAppSelector } from './app/hooks';
 
-export const App = () => (
-  <>
-    <div className="section">
-      <div className="container">
-        <div className="box">
-          <h1 className="title">Todos:</h1>
+export const App = () => {
+  const todos = useAppSelector(state => state.todos);
+  const selectedTodo = useAppSelector(state => state.currentTodo);
+  const filter = useAppSelector(state => state.filter);
+  const dispatch = useAppDispatch();
 
-          <div className="block">
-            <TodoFilter />
-          </div>
+  const [isLoading, setIsLoading] = useState(false);
 
-          <div className="block">
-            <Loader />
-            <TodoList />
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos()
+      .then(response => dispatch(todosSlice.actions.setTodos(response)))
+      .finally(() => setIsLoading(false));
+  }, [dispatch]);
+
+  const visibleTodos = todos
+    .filter(todo =>
+      todo.title.toLowerCase().includes(filter.query.toLowerCase()),
+    )
+    .filter(todo => {
+      if (filter.status === 'active') {
+        return !todo.completed;
+      }
+
+      if (filter.status === 'completed') {
+        return todo.completed;
+      }
+
+      return true;
+    });
+
+  return (
+    <>
+      <div className="section">
+        <div className="container">
+          <div className="box">
+            <h1 className="title">Todos:</h1>
+
+            <div className="block">
+              <TodoFilter />
+            </div>
+
+            <div className="block">
+              {isLoading ? <Loader /> : <TodoList todos={visibleTodos} />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-
-    <TodoModal />
-  </>
-);
+      {selectedTodo && <TodoModal todo={selectedTodo} />}
+    </>
+  );
+};
